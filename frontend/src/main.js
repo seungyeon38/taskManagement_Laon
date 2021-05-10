5bca42598d48db9a9f88821ae56f522a0dad80cc
seungyeon <seongyeon38@gmail.com>
2021-05-10 10:46:00 +0900

    main.js

---
+++ frontend/src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import vueMoment from 'vue-moment'
+
+
+Vue.prototype.$axios = axios;
+Vue.config.productionTip = false
+
+
+Vue.use(Element);
+Vue.use(vueMoment);
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+
